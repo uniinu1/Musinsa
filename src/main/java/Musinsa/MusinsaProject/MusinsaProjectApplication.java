3163e86c8be5f8b55e8b57c5f57f384e4b3b0082
uniinu1 <qkrtnals3636@likelion.org>
2024-09-13 13:54:10 +0900

    Spring Boot Project Commit

---
+++ src/main/java/Musinsa/MusinsaProject/MusinsaProjectApplication.java
@@ -0,0 +1,13 @@
+package Musinsa.MusinsaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Musinsa/MusinsaProject/MusinsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Musinsa.MusinsaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
